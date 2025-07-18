afcd49615fbcfdcd7d3da09407dee11d9695bcb3
ElKev118 <kevin.ruda@udea.edu.co>
2025-07-17 22:35:01 -0500

    first commit

---
+++ src/test/java/com/udea/Parcial_2_Arq_Soft_Back/Parcial2ArqSoftBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.Parcial_2_Arq_Soft_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ArqSoftBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
